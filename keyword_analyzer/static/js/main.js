6278216175a81e52d51aec40338eb98e9b746898
RubelBiswasCS <rubelbiswas36@gmail.com>
2022-03-06 20:03:00 +0600

    modified fronted"

--- keyword_analyzer/static/js/main.js
+++ keyword_analyzer/static/js/main.js
@@ -162,3 +162,4 @@ const filterKeywords = (keywords) => {
 
     return filteredKeywords;
 }
+
---
+++ keyword_analyzer/static/js/search.js
@@ -0,0 +1,21 @@
+
+const clearBtn=document.getElementById("clear")
+const textBox=document.getElementById("search-text")
+if(textBox.value===''){
+    clearBtn.style.visibility = 'hidden';
+}
+textBox.addEventListener('input',()=> {
+    if(textBox.value===''){
+        clearBtn.style.visibility = 'hidden';
+    }
+    else{
+        clearBtn.style.visibility = 'visible';
+    }
+})
+const handleClearBtnClick = (e) => {
+    e.preventDefault();
+    textBox.value='';
+    clearBtn.style.visibility = 'hidden';
+}
+clearBtn.addEventListener('click',handleClearBtnClick)
+
